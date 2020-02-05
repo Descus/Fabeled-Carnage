69d5f8c626ee1f5b55c504158d7951ad13d0b22d
Descus <iDescusi@gmail.com>
2020-02-05 14:12:29 +0100

    Score Now Working

--- Assets/Scripts/Rewrite/GameObjects/Actors/Enemies/Goose.cs
+++ Assets/Scripts/Rewrite/GameObjects/Actors/Enemies/Goose.cs
@@ -4,10 +4,18 @@ namespace Rewrite.GameObjects.Actors.Enemies
 {
     public class Goose: Animal
     {
+        private float _yPos;
+        new void Start()
+        {
+            _yPos = transform.position.y;
+            base.Start();
+        }
+        
+        
         new void Update()
         {
-            var position = transform.position;
-            position = new Vector3(position.x, position.y + 0.8f * Mathf.Sin(Time.time), position.z);
+            Vector3 position = transform.position;
+            position = new Vector3(position.x, _yPos + 0.08f * Mathf.Sin(4 * Time.time), position.z);
             transform.position = position;
             base.Update();
         }
